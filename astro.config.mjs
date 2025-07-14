70939ab566c7513fc7b38ad3c0c9cdd98c6edee7
MicahNyakangoMosomi <nyakangomicah4@gmail.com>
2025-07-15 01:56:32 +0300

    fixs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
+  output: 'static',
+  base: '/',
   // site: 'https://porfolio.dev/'
 })
